689ee78bf492feea35cf73774fc4209c3458d0c7
rebeccamansson <97900349+rebeccamansson@users.noreply.github.com>
2022-05-03 11:34:51 +0200

    created header

---
+++ header.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function buildHeader() {
+    let body = document.body;
+    let headerDiv = document.createElement("div");
+    headerDiv.classList.add("header");
+
+    body.append(headerDiv);
+
+    let logga = document.createElement("img")
+    logga.src = "images/logga.svg";
+    logga.classList.add("logga")
+    headerDiv.append(logga);
+
+    let hamburgerMenu = document.createElement("img")
+    hamburgerMenu.src = "images/Hamburger_icon.png";
+    hamburgerMenu.classList.add("hamburger");
+    headerDiv.append(hamburgerMenu);
+
+    return headerDiv;
+}
