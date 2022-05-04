51c863f76bfc5eda7ed43ebef8c0f3e2a0e2c16e
claralindbloms <97987325+claralindbloms@users.noreply.github.com>
2022-05-04 11:41:59 +0200

    Update BurgerMenu.js

--- JS/BurgerMenu.js
+++ JS/BurgerMenu.js
@@ -1,1 +1,26 @@
 "use strict";
+
+/*function buildHeader ()
+createDiv headerHTML
+append logo create <A> 
+append burgerMenuIcon
+
+return headerHTML
+
+
+function buildBurgerMenu ()
+createDiv burgerHTML
+
+append title, darkMode, close
+
+array burgerElements
+text: “Hitta din utbildning”, url: path_to_page_Hitta
+text: “Destinationer”, url:...
+text: “”, url:...
+
+loop through array
+	create<A> burgerElementHMTL
+	fix text and href in burgerElementHMTL
+	append burgerElementHTML to burgerHTML
+
+return burgerHTML*/
