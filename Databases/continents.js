e04ac3aabe2717eafddc97bd9d53ac93f041a000
hedvigforsberg <hedvigforsberg@gmail.com>
2022-05-04 11:22:35 +0200

    Created database-file for Continents
    Contains each continent with name and their corresponding countries with ID.

---
+++ Databases/continents.js
@@ -0,0 +1,32 @@
+const CONTINENTS = [
+  {
+    name: "Africa",
+    countries: [],
+  },
+
+  {
+    name: "Asia",
+    countries: [],
+  },
+
+  {
+    name: "Europe",
+    countries: [0, 1, 3, 4],
+  },
+
+  {
+    name: "North America",
+    countries: [5, 6]
+  },
+
+  {
+    name: "Oceania",
+    countries: [2],
+  },
+
+  {
+    name: "South America",
+    countries: [7, 8],
+  },
+];
+
