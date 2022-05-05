5cd4f60e52a59cfc78457728abf6ec920b25d695
hedvigforsberg <hedvigforsberg@gmail.com>
2022-05-05 14:57:44 +0200

    Continued with buildContinents()
    Added test.html (to be deleted)

--- JS/buildContinents.js
+++ JS/buildContinents.js
@@ -4,40 +4,46 @@ const CONTINENTS = [
     {
       name: "Afrika",
       countries: [],
+      image: "africa.png"
     },
   
     {
       name: "Asien",
       countries: [],
+      image: "asia.png"
     },
   
     {
       name: "Europa",
       countries: [0, 1, 3, 4],
+      image:"europe.png"
     },
   
     {
       name: "Nordamerika",
-      countries: [5, 6]
+      countries: [5, 6],
+      image:"north_america.png"
     },
   
     {
       name: "Oceanien",
       countries: [2],
+      image:"oceania.png"
     },
   
     {
       name: "Sydamerika",
       countries: [7, 8],
+      image:"south_america.png"
     },
   ];
   
 function buildContinents () {
-    
     for (let i = 0; i < CONTINENTS.length; i++) {
         
         let continent = document.createElement("div");
         continent.innerHTML = `
+            <img src="./images/${CONTINENTS[i].image}"</img>
             <h2>${CONTINENTS[i].name}</h2>
         `
         let testDiv = document.getElementById("testDiv")
@@ -46,8 +52,6 @@ function buildContinents () {
 }
 
 buildContinents();
-
-
 
 
 // function buildContinents ()
