e40e3785100fa13e0bdf6dc559a99842c491c5ad
rebeccamansson <97900349+rebeccamansson@users.noreply.github.com>
2022-05-10 13:22:48 +0200

    fixes

--- JS/buildCountryHomeoverlay.js
+++ JS/buildCountryHomeoverlay.js
@@ -1,6 +1,6 @@
 // function buildCountryHomeOverlay () {
 //     for (let i = 0; i < COUNTRIES.length; i++) {
-        
+
 //         let country = document.createElement("div");
 //         country.innerHTML = `
 //             <img class="country_images" src="./images/${COUNTRIES[i].imagesNormal[1]}"</img>
@@ -16,25 +16,22 @@ buildCountryHomeOverlay();
 
 function on() {
     document.getElementById("overlay").style.display = "block";
+
     for (let i = 0; i < COUNTRIES.length; i++) {
-        
+
         let country = document.createElement("div");
         country.innerHTML = `
+            <h2 class="country_header">${COUNTRIES[i].name}</h2>
             <img class="country_images" src="./images/${COUNTRIES[i].imagesNormal[1]}"</img>
-            <h2>${COUNTRIES[i].name}</h2>
+           
         `
         let globesContainer = document.getElementById("globes");
         globesContainer.append(country);
     }
 }
 
-  function off() {
-
+function off() {
     document.getElementById("overlay").style.display = "none";
-    let text = document.createElement ("p");
-    text.innerHTML ="Global Studies är en hemsida som skapades 2022 av fyra studenter Global Studies är en hemsida som skapades 2022 av fyra studenter frå"
-
-        let globesContainer = document.getElementById("globes");
-        globesContainer.append(country);
-  
-  }
+    let body = document.getElementById ("wrapper");
+    body.innerHTML =""
+}
--- JS/header.js
+++ JS/header.js
@@ -9,12 +9,12 @@ function buildHeader() {
 
 
     let logga = document.createElement("img")
-    logga.src = "../images/logga.svg";
+    logga.src = "./images/logga.svg";
     logga.classList.add("logga")
     headerDiv.append(logga);
 
     let hamburgerMenu = document.createElement("img")
-    hamburgerMenu.src = "../images/Hamburger_icon.png";
+    hamburgerMenu.src = "./images/Hamburger_icon.png";
     hamburgerMenu.classList.add("hamburger");
     headerDiv.append(hamburgerMenu);
 
