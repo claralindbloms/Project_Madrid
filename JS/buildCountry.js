0c187e4f383222005ffc9393cb2364d85d13cbdc
hedvigforsberg <hedvigforsberg@gmail.com>
2022-05-10 15:04:32 +0200

    Testing

--- JS/buildCountry.js
+++ JS/buildCountry.js
@@ -1,65 +1,78 @@
 "use strict";
 
-// funktion som ska skapa en informationsruta om vardera land
+function sumGradeCourses() {
+  let gradeCourses = [];
 
-function buildCountry (country){
+  for (let i = 0; i < COMMENTS_PROGRAMME.length; i++) {
+    gradeCourses.push(COMMENTS_PROGRAMME[i].stars.courses);
+  }
 
- 
-    let countryResult = document.getElementById("countryWrapper");
-    let countryContainer = document.createElement("div");
-    countryContainer.classList.add("countryContainer");
- 
-    let countryName = document.createElement("h2");
-    
-    countryName.innerText = country.flag + " " + country.name;
-    countryContainer.append(countryName);
+  let sumGradeCourses = 0;
 
-    let countryImage = document.createElement("img");
-    countryImage.src = "../images/" + country.imagesNormal[0]; 
-    /* hur får jag så rätt bild väljs till rätt land? */ 
+  for (let i = 0; i < gradeCourses.length; i++) {
+    sumGradeCourses += gradeCourses[i];
+  }
 
-    let aboutCountry = document.createElement("p");
-    aboutCountry.innerText = country.text; 
-    countryContainer.append(aboutCountry);
-        
-    let visaCountry = document.createElement("p");
-    if (country.visa === false) {
-        visaCountry.innerText = "Visum: " + "Nej";
-    } else {
-        visaCountry.innerText = "Visum: " + "Ja";
-    }
-    countryContainer.append(visaCountry);
+  let averageGradeCourses = sumGradeCourses / gradeCourses.length;
 
-    let foundLanguages = getLanguageById(country);
-    let languageCountry = document.createElement("p");
-    languageCountry.innerText = "Språk: " + foundLanguages[0];
-    // behövs en loop till för att få rätt språk mha id? // 
-        
-    countryResult.append(countryContainer);
-
-    let toEduButton = document.createElement("button");
-
-    // let selectCity = document.createElement("select"); //
-
-   
+  return Math.round(averageGradeCourses * 10) / 10;
 }
 
-for (let i = 0; i < COUNTRIES.length; i++){
-    buildCountry (COUNTRIES[i]);
+// funktion som ska skapa en informationsruta om vardera land
+function buildCountry(country) {
+  let countryResult = document.getElementById("countryWrapper");
+  let countryContainer = document.createElement("div");
+  countryContainer.classList.add("countryContainer");
+
+  let countryName = document.createElement("h2");
+
+  countryName.innerText = country.flag + " " + country.name;
+  countryContainer.append(countryName);
+
+  let countryImage = document.createElement("img");
+  countryImage.src = "../images/" + country.imagesNormal[0];
+  /* hur får jag så rätt bild väljs till rätt land? */
+
+  let aboutCountry = document.createElement("p");
+  aboutCountry.innerText = country.text;
+  countryContainer.append(aboutCountry);
+
+  let visaCountry = document.createElement("p");
+  if (country.visa === false) {
+    visaCountry.innerText = "Visum: " + "Nej";
+  } else {
+    visaCountry.innerText = "Visum: " + "Ja";
+  }
+  countryContainer.append(visaCountry);
+
+  let foundLanguages = getLanguageById(country);
+  let languageCountry = document.createElement("p");
+  languageCountry.innerText = "Språk: " + foundLanguages[0];
+  // behövs en loop till för att få rätt språk mha id? //
+
+  countryResult.append(countryContainer);
+
+  let toEduButton = document.createElement("button");
+
+  // let selectCity = document.createElement("select"); //
+}
+
+for (let i = 0; i < COUNTRIES.length; i++) {
+  buildCountry(COUNTRIES[i]);
 }
 
 // Hitta rätt språk baserat på dess id
-function getLanguageById (country) {
-    let foundLanguages = []
-  
-    for (let i = 0; i < country.length; i++) {
-      foundLanguages.push(
-        country.LANGUAGE.find(language => {
-          return language.id == country.languageID
-        })
-      )
-    }
-    return foundLanguages;
+function getLanguageById(country) {
+  let foundLanguages = [];
+
+  for (let i = 0; i < country.length; i++) {
+    foundLanguages.push(
+      country.LANGUAGE.find((language) => {
+        return language.id == country.languageID;
+      })
+    );
+  }
+  return foundLanguages;
 }
 
 //   const LANGUAGES = [
@@ -85,19 +98,18 @@ function getLanguageById (country) {
 //     }
 // denna ska köras i eventlyssnaren för select-funktionen
 
-function createHTML (countries) {
-    for (let country of countries) {
-        buildCountry(country)
-    }
+function createHTML(countries) {
+  for (let country of countries) {
+    buildCountry(country);
+  }
 }
-  
 
 // Måste ha något sånt här för när användaren väljer ett land i select
 // inputStudent.addEventListener('keyup', function () {
 //     let foundStudent = findStudent()
 //     document.getElementById('results').innerHTML = ''
 //     createHTML(foundStudent)
-  
+
 //     if (inputStudent.value == '') {
 //       document.getElementById('results').innerHTML = ''
 //     }
