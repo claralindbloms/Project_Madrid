5a77988b17ca127a84e21ae469a5e41f8a07d5fd
Fanny Nilsson <fannyy.nilsson@gmail.com>
2022-05-17 14:04:18 +0200

    Attached comments in buildCity
    Does not work yet

--- JS/buildCity.js
+++ JS/buildCity.js
@@ -1,21 +1,20 @@
-"use strict";
+'use strict'
 // för att städerna inte ska visas direkt när destinations-sidan öppnas
 
 // let countryResult = document.getElementById("countryWrapper");
 // countryResult.innerHTML = "";
 
 function buildCity (city) {
+  let countryResult = document.getElementById('countryWrapper')
+  let cityContainer = document.createElement('div')
+  cityContainer.classList.add('cityContainer')
 
-    let countryResult = document.getElementById("countryWrapper");
-    let cityContainer = document.createElement("div");
-    cityContainer.classList.add("cityContainer");
+  let foodGrade = averageGradeCity(city, 'food')
+  let accomodationGrade = averageGradeCity(city, 'accomodation')
+  let outGrade = averageGradeCity(city, 'out')
 
-    let foodGrade = averageGradeCity(city, "food");
-    let accomodationGrade = averageGradeCity(city, "accomodation");
-    let outGrade = averageGradeCity(city, "out");
-
-    // let comments = getComments(city);
-    cityContainer.innerHTML = `
+  // let comments = getComments(city);
+  cityContainer.innerHTML = `
     <h2>${city.name}</h2>
     <img class="cityImage" src="./images/${city.imagesNormal[0]}">
     <p>${city.text}</p>
@@ -37,61 +36,113 @@ function buildCity (city) {
 
     <div id="comments">
         <h3>Kommentarer</h3>
-        
+        <div>${comments}</div> 
+        <button class="mooreComments">Visa fler kommentarer</button>
     </div>
     <button>Till utbildningar</button>
-    `;
+    `
 
+  // div för kommentarer, två nyaste visar, sen visa mer - Clara
 
-
-    // div för kommentarer, två nyaste visar, sen visa mer - Clara
-
-
-
-    countryResult.append(cityContainer);
+  countryResult.append(cityContainer)
 }
 
-
-
-for (let i = 0; i < CITIES.length; i++){
-    buildCity (CITIES[i]);
+for (let i = 0; i < CITIES.length; i++) {
+  buildCity(CITIES[i])
 }
 
 function getCitiesByCountryId (city) {
-    let foundCities = [];
+  let foundCities = []
 
-    for (let i = 0; i < COUNTRIES.length; i++) {
-        for (let i = 0; i < CITIES.length; i++) {
-            if (city.id == CITIES[i].countryID) {
-                foundCities.push(CITIES[i].name);
-            }
-        }
+  for (let i = 0; i < COUNTRIES.length; i++) {
+    for (let i = 0; i < CITIES.length; i++) {
+      if (city.id == CITIES[i].countryID) {
+        foundCities.push(CITIES[i].name)
+      }
     }
-    return foundCities;
+  }
+  return foundCities
 }
 
 function averageGradeCity (city, type) {
-    let grade = [];
-  
-    COMMENTS_CITY.filter((comment) => {
-      if (comment.cityID == city.id) {
-        grade.push(comment.stars[type]);
-      }
-    });
-  
-    return averageCalc(grade);
+  let grade = []
+
+  COMMENTS_CITY.filter(comment => {
+    if (comment.cityID == city.id) {
+      grade.push(comment.stars[type])
+    }
+  })
+
+  return averageCalc(grade)
+}
+
+function averageCalc (array) {
+  let sum = 0
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i]
   }
 
-function averageCalc(array) {
-    let sum = 0;
-  
-    for (let i = 0; i < array.length; i++) {
-      sum += array[i];
+  let average = sum / array.length
+
+  let averageGrade = Math.round(average * 10) / 10
+
+  return averageGrade
+}
+
+let orderedComments
+let showComments = 0
+
+function init (city) {
+  orderedComments = COMMENTS_CITY.filter(
+    comment => comment.cityID === city.id
+  ).sort((comment1, comment2) => compareByDate(comment2.date, comment1.date))
+}
+
+function compareByDate (date1, date2) {
+  if (date1.year > date2.year) {
+    return 1
+  } else if (date1.year < date2.year) {
+    return -1
+  } else {
+    if (date1.month > date2.month) {
+      return 1
+    } else if (date1.month < date2.month) {
+      return -1
+    } else {
+      if (date1.day > date2.day) {
+        return 1
+      } else if (date1.day < date2.day) {
+        return -1
+      } else {
+        return 0
+      }
     }
-  
-    let average = sum / array.length;
-  
-    let averageGrade = Math.round(average * 10) / 10;
-  
-    return averageGrade;
   }
+}
+
+function getComments (numberOfComments) {
+  for (let i = showComments; i < showComments + numberOfComments; i++) {
+    if (i === orderedComments.length) {
+      break
+    }
+    let comment = orderedComments[i]
+    let div = document.createElement('div')
+    div.classList.add('comment')
+    div.innerHTML = `<p>${comment.alias}, ${comment.date.year}</p>
+        <p>"${comment.text}"</p>`
+    // let box = document.getElementById("box"); //TO DO, get correct element
+    // box.append(div);
+  }
+  showComments += numberOfComments
+}
+
+//function //TO DO button, skapa en egen funtion för detta?
+let mooreComments = document.getElementsByClassName('.mooreComments')
+
+mooreComments.addEventListener('click', function () {
+  getComments(2)
+})
+
+init(city) //TO DO, correct city ID, dont have a number in init
+getComments(2)
