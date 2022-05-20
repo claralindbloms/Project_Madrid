6903d8b62906b2c411640f9c6c4990627c89fa8d
Fanny Nilsson <fannyy.nilsson@gmail.com>
2022-05-20 13:39:04 +0200

    Added comments to chooseCountry

--- JS/chooseCountry.js
+++ JS/chooseCountry.js
@@ -1,12 +1,16 @@
 "use strict";
 
-// länka länderna till select
+// this code (5-33) finds the countries and adds them to the chooseCountry-selector
 
 let chooseCountry = document.getElementById("chooseCountry");
+
+// this part removes the earlier choosen value in the selector
 
 window.onload = function () {
   chooseCountry.value = "";
 };
+
+// this part creates the options in the select-element
 
 COUNTRIES.forEach((country) => {
   let option = document.createElement("option");
@@ -14,6 +18,8 @@ COUNTRIES.forEach((country) => {
 
   chooseCountry.append(option);
 });
+
+// event-listener to change between the countries and build the choosen countrys "information"-div
 
 chooseCountry.addEventListener("change", function (event) {
   const country = COUNTRIES.find(function (c) {
