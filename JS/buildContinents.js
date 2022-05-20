e23a8ebfe4d4f0a44aabaedc3d46aca8ba2fd0a7
Fanny Nilsson <fannyy.nilsson@gmail.com>
2022-05-20 13:30:39 +0200

    Disclaimer when no countries
    And some formatting and comments

--- JS/buildContinents.js
+++ JS/buildContinents.js
@@ -1,82 +1,97 @@
-"use strict";
+'use strict'
 
-function buildContinents() {
+function buildContinents () {
   for (let i = 0; i < CONTINENTS.length; i++) {
-    let continent = document.createElement("div");
+    let continent = document.createElement('div')
     continent.innerHTML = `
             <img class="continent_images" src="./images/${CONTINENTS[i].image}"</img>
             <h2> ${CONTINENTS[i].name}</h2>
-        `;
+        `
     if (CONTINENTS[i].countries == 0) {
-      continent.style.opacity = "0.4";
+      continent.style.opacity = '0.4'
     }
 
-    let globesContainer = document.getElementById("globes");
-    globesContainer.append(continent);
+    let globesContainer = document.getElementById('globes')
+    globesContainer.append(continent)
 
-    continent.addEventListener("click", function (event) {
-      let foundC = getCountryByContinentId(CONTINENTS[i].id);
-      let popupHTML = document.querySelector(".continent_images");
-      if (document.querySelector(".menuactive")) {
-        let overlay = document.querySelector(".overlay");
-        popupHTML.classList.remove("menuactive");
-        overlay.remove();
+    continent.addEventListener('click', function (event) {
+      let foundC = getCountryByContinentId(CONTINENTS[i].id)
+      console.log(foundC)
+      let popupHTML = document.querySelector('.continent_images')
+      if (document.querySelector('.menuactive')) {
+        let overlay = document.querySelector('.overlay')
+        popupHTML.classList.remove('menuactive')
+        overlay.remove()
       } else {
-        let burgerHTML = document.querySelector(".hamburger");
-        burgerHTML.style.display = "none";
-        let overlay = document.createElement("div");
-        overlay.classList.add("overlay");
-        popupHTML.classList.add("menuactive");
-        let close = document.createElement("a");
-        close.classList.add("close");
-        close.href = "#";
-        overlay.append(close);
+        let burgerHTML = document.querySelector('.hamburger')
+        burgerHTML.style.display = 'none'
+        let overlay = document.createElement('div')
+        overlay.classList.add('overlay')
+        popupHTML.classList.add('menuactive')
 
-        close.addEventListener("click", function (event) {
-          window.location.href = "./index.html";
-        });
+        // creates and appends a close-icon on the country pop-up
+        let close = document.createElement('a')
+        close.classList.add('close')
+        close.href = '#'
+        overlay.append(close)
 
-        let wrap = document.querySelector("#wrapper");
-        wrap.append(overlay);
+        // if-statement to deliver message about no current educations in choosen continent
 
-        foundC.forEach((country) => {
-          popupCountries(country, overlay);
-        });
+        if (foundC.length === 0) {
+          let sorry = document.createElement('p')
+          sorry.classList.add('sorry')
+          sorry.innerText =
+            'Tyvärr finns inga utbildningar tillgängliga i' +
+            ` ${CONTINENTS[i].name}`
+          overlay.append(sorry)
+        }
+
+        // event-listener that redirects the user to the index.html page
+        close.addEventListener('click', function (event) {
+          window.location.href = './index.html'
+        })
+
+        let wrap = document.querySelector('#wrapper')
+        wrap.append(overlay)
+
+        foundC.forEach(country => {
+          popupCountries(country, overlay)
+        })
       }
-    });
+    })
   }
 }
 
-function getCountryByContinentId(id) {
-  let foundCountry = [];
+function getCountryByContinentId (id) {
+  let foundCountry = []
 
   for (let f = 0; f < COUNTRIES.length; f++) {
     if (id == COUNTRIES[f].contitentId) {
-      foundCountry.push(COUNTRIES[f]);
+      foundCountry.push(COUNTRIES[f])
     }
   }
-  return foundCountry;
+  return foundCountry
 }
 
-function popupCountries(country, overlay) {
-  let countries = document.createElement("div");
-  countries.classList.add("country");
+function popupCountries (country, overlay) {
+  let countries = document.createElement('div')
+  countries.classList.add('country')
 
   countries.innerHTML = `
-            <img class="country_images" src="./images/${country.imagesNormal[0]}"</img>
-            <h2>${country.name}</h2>
-        `;
+  <img class="country_images" src="./images/${country.imagesNormal[0]}"</img>
+  <h2>${country.name}</h2>
+  `
 
-  countries.addEventListener("click", function () {
-    sessionStorage.setItem("country", JSON.stringify(country));
-    window.location.href = "./destinationer.html";
-  });
+  countries.addEventListener('click', function () {
+    sessionStorage.setItem('country', JSON.stringify(country))
+    window.location.href = './destinationer.html'
+  })
 
-  overlay.append(countries);
+  overlay.append(countries)
 }
 
-buildContinents();
+buildContinents()
 
-document.getElementById("bookicon").onclick = function () {
-  location.href = "utbildningar.html";
-};
+document.getElementById('bookicon').onclick = function () {
+  location.href = 'utbildningar.html'
+}
